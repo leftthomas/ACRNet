b064330bf2573264bc5a04d9a07d4d490bc0e04d
leftthomas <leftthomas@qq.com>
2022-11-11 20:32:04 +0800

    simplified code

--- dataset.py
+++ dataset.py
@@ -6,54 +6,28 @@ import utils
 
 
 class SampleDataset:
-    def __init__(self, args, mode="both", sampling='random'):
+    def __init__(self, args):
         self.dataset_name = args.dataset_name
         self.num_class = args.num_class
-        self.sampling = sampling
         self.num_segments = args.max_seqlen
         self.feature_size = args.feature_size
         self.path_to_features = os.path.join(args.path_dataset, self.dataset_name + "-I3D-JOINTFeatures.npy")
         self.path_to_annotations = os.path.join(args.path_dataset, self.dataset_name + "-Annotations/")
-        self.features = np.load(
-            self.path_to_features, encoding="bytes", allow_pickle=True
-        )
-        self.segments = np.load(
-            self.path_to_annotations + "segments.npy", allow_pickle=True
-        )
-        self.labels = np.load(
-            self.path_to_annotations + "labels_all.npy", allow_pickle=True
-        )
-        # Specific to Thumos14
-
-        self._labels = np.load(
-            self.path_to_annotations + "labels.npy", allow_pickle=True
-        )
-        self.classlist = np.load(
-            self.path_to_annotations + "classlist.npy", allow_pickle=True
-        )
-        self.subset = np.load(
-            self.path_to_annotations + "subset.npy", allow_pickle=True
-        )
-        self.videonames = np.load(
-            self.path_to_annotations + "videoname.npy", allow_pickle=True
-        )
+        self.features = np.load(self.path_to_features, encoding="bytes", allow_pickle=True)
+        self.segments = np.load(self.path_to_annotations + "segments.npy", allow_pickle=True)
+        self.labels = np.load(self.path_to_annotations + "labels_all.npy", allow_pickle=True)
+        self._labels = np.load(self.path_to_annotations + "labels.npy", allow_pickle=True)
+        self.classlist = np.load(self.path_to_annotations + "classlist.npy", allow_pickle=True)
+        self.subset = np.load(self.path_to_annotations + "subset.npy", allow_pickle=True)
+        self.videonames = np.load(self.path_to_annotations + "videoname.npy", allow_pickle=True)
         self.batch_size = args.batch_size
         self.trainidx = []
         self.testidx = []
         self.classwiseidx = []
         self.currenttestidx = 0
-        self.labels_multihot = [
-            utils.strlist2multihot(labs, self.classlist)
-            for labs in self.labels
-        ]
+        self.labels_multihot = [utils.strlist2multihot(labs, self.classlist) for labs in self.labels]
         self.train_test_idx()
         self.classwise_feature_mapping()
-
-        self.normalize = False
-        self.mode = mode
-        if mode == "rgb" or mode == "flow":
-            self.feature_size = 1024
-
         if args.dataset_name == 'ActivityNet1.2':
             self.filter()
 
@@ -75,7 +49,7 @@ class SampleDataset:
     def train_test_idx(self):
         if self.dataset_name == 'Thumos14reduced':
             for i, s in enumerate(self.subset):
-                if s.decode("utf-8") == "validation":  # Specific to Thumos14
+                if s.decode("utf-8") == "validation":
                     self.trainidx.append(i)
                 elif s.decode("utf-8") == "test":
                     self.testidx.append(i)
@@ -104,16 +78,9 @@ class SampleDataset:
             idx = []
             # Load similar pairs
             if n_similar != 0:
-                rand_classid = np.random.choice(
-                    len(self.classwiseidx), size=n_similar
-                )
+                rand_classid = np.random.choice(len(self.classwiseidx), size=n_similar)
                 for rid in rand_classid:
-                    rand_sampleid = np.random.choice(
-                        len(self.classwiseidx[rid]),
-                        size=similar_size,
-                        replace=False,
-                    )
-
+                    rand_sampleid = np.random.choice(len(self.classwiseidx[rid]), size=similar_size, replace=False)
                     for k in rand_sampleid:
                         idx.append(self.classwiseidx[rid][k])
 
@@ -121,32 +88,18 @@ class SampleDataset:
             if self.batch_size - similar_size * n_similar < 0:
                 self.batch_size = similar_size * n_similar
 
-            rand_sampleid = np.random.choice(
-                len(self.trainidx),
-                size=self.batch_size - similar_size * n_similar,
-            )
+            rand_sampleid = np.random.choice(len(self.trainidx), size=self.batch_size - similar_size * n_similar)
 
             for r in rand_sampleid:
                 idx.append(self.trainidx[r])
             feat = []
             for i in idx:
                 ifeat = self.features[i]
-                if self.sampling == 'random':
-                    sample_idx = self.random_sampling(ifeat.shape[0])
-                elif self.sampling == 'uniform':
-                    sample_idx = self.uniform_sampling(ifeat.shape[0])
-                elif self.sampling == "all":
-                    sample_idx = np.arange(ifeat.shape[0])
-                else:
-                    raise AssertionError('Not supported sampling !')
+                sample_idx = self.random_sampling(ifeat.shape[0])
                 ifeat = ifeat[sample_idx]
                 feat.append(ifeat)
             feat = np.array(feat)
             labels = np.array([self.labels_multihot[i] for i in idx])
-            if self.mode == "rgb":
-                feat = feat[..., : self.feature_size]
-            elif self.mode == "flow":
-                feat = feat[..., self.feature_size:]
             return feat, labels, rand_sampleid
 
         else:
@@ -160,10 +113,6 @@ class SampleDataset:
                 done = False
                 self.currenttestidx += 1
             feat = np.array(feat)
-            if self.mode == "rgb":
-                feat = feat[..., : self.feature_size]
-            elif self.mode == "flow":
-                feat = feat[..., self.feature_size:]
             return feat, np.array(labs), vn, done
 
     def random_sampling(self, num_seg):
@@ -175,9 +124,3 @@ class SampleDataset:
                 sample_idx[i] = np.random.randint(int(sample_idx[i]), int(sample_idx[i + 1]))
         return sample_idx[:-1].astype(np.int)
 
-    def uniform_sampling(self, length):
-        # because the length may different as these two line codes, make sure batch size == 1 in test mode
-        if length <= self.num_segments:
-            return np.arange(length).astype(np.int)
-        else:
-            return np.floor(np.arange(self.num_segments) * length / self.num_segments).astype(np.int)
